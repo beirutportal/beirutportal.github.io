2694a6997047ee6a107495feccbf3711c60fb6e0
beirutportal <69352752+beirutportal@users.noreply.github.com>
2020-08-08 14:59:23 +0300

    JS Update

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+import * as Donations from './js/donations.js';
+
+
 $(document).ready(function(){
+
+    $('#Donations .dn-title').children('h5').text(Donations.list.length + " DONATION LINKS");
+
 
     $('#simplifylist').click(function(){
         if( $('#simplifylist').is(":checked") ){
@@ -15,6 +21,57 @@ $(document).ready(function(){
     $('.update-notice-icon').click(function(){
         $('.update-notice').css({'display':'none'});
     });
+
+
+
+    
+
+
+
+
+    //Donations List Builder
+    for(var i = 0; i<Donations.list.length; i++){
+
+        //Card Builder
+        let $card = $("<div class='card'></div>");
+        let $img = $("<img src='"+ Donations.list[i].imgsrc + "' class='card-img-top'>");
+        let $cardbody = $("<div class='card-body'></div>");
+        let $cardtitle = $("<h5 class='card-title'>"+ Donations.list[i].name +"</h5>");
+        let $cardtext = $("<p class='card-text'>" + Donations.list[i].excerpt + "</p>");
+
+        let $donateurl = $("<a href=" + Donations.list[i].donateurl + " target='_blank'></a>");
+        let $btndonate = $("<button type='button' class='btn btn-success green'>DONATE NOW <i class='fa fa-external-link' aria-hidden='true'></i></button>");
+
+        let $weburl = $("<a href=" + Donations.list[i].weburl + " target='_blank'></a>");
+        let $btnurl = $("<button type='button' class='btn btn-outline-secondary'>Learn more <i class='fa fa-external-link' aria-hidden='true'></i></button>");
+
+        $('.card-columns').append($card);
+        $card.append($img);
+		$card.append($cardbody);
+        $cardbody.append($cardtitle);
+        $cardbody.append($cardtext);
+        
+        $cardbody.append($donateurl);
+        $donateurl.append($btndonate);
+
+        $cardbody.append($weburl);
+        $weburl.append($btnurl);
+
+
+
+        //Simplified List Builder
+        let $elmt = $("<div class='elmt'></div>");
+        let $p = $("<p></p>");
+        let $elmtlink = $("<a href='" + Donations.list[i].donateurl + "' target='_blank'> " + Donations.list[i].name + "<i class='fa fa-external-link' aria-hidden='true'></i></a>")
+
+        $('.elmts-collection').append($elmt);
+        $elmt.append($p);
+        $p.append($elmtlink);
+        
+
+
+    }
+
 
 
 
---
+++ js/donations.js
@@ -0,0 +1,142 @@
+export const list = [
+
+    {
+        id: 0,
+        name: "Lebanese Red Cross",
+        excerpt: "The Lebanese Red Cross (LRC) is a national society that was established on July 9, 1945 as an independent National Society. In 1946, it was recognized by the State as a public nonprofit organization and as an auxiliary team to the medical personnel of the Lebanese Army...",
+        donateurl: "https://www.supportlrc.app/",
+        weburl: "http://www.redcross.org.lb/index.aspx?pageid=907",
+        imgsrc: "assets/imgs/donations/lrc.gif"
+    }
+    ,
+    {
+        id: 1,
+        name: "Donner Sans Compter",
+        excerpt: "DSC is a non-governmental, non-profit organization founded onthe principles of empathy,compassion, humility, and sacrifice. We are not politically aligned. We are notreligiously affiliated. Ouronly commitment is to humanity. DSC links patients in need of blood donationsand those who are willing todonate...",
+        donateurl: "https://www.givingloop.org/dsc",
+        weburl: "https://dsclebanon.org/",
+        imgsrc: "assets/imgs/donations/dsc.jpg"
+    }
+    ,
+    {
+        id: 2,
+        name: "arcenciel",
+        excerpt: `Adhering to the principles of sustainable development, 
+        arcenciel emphasizes the social and economic integration of marginalized people and communities back into society. 
+        It believes that every person, no matter the gravity of their handicap, 
+        is capable of overcoming their disability and contributing to the communities in which they live. 
+        arcenciel supports the diversity of society, 
+        and encourages the sustainability and conservation of natural resources in all aspects of its work...`,
+        donateurl: "https://www.arcenciel.org/donate/",
+        weburl: "https://www.arcenciel.org/en/",
+        imgsrc: "assets/imgs/donations/aec.jpg"
+    },
+    {
+        id: 3,
+        name: "Impact Lebanon",
+        excerpt: `Impact Lebanon is a non-profit organisation that brings the community together to pursue initiatives that deliver impact for Lebanon. 
+        Our process is to encourage the brainstorming of ideas; 
+        promote the sharing of knowledge, resources and expertise; 
+        and build strong teams to turn an idea into a successful initiative...`,
+        donateurl: "https://www.justgiving.com/crowdfunding/lebanon-relief?utm_term=PYp7gXzyD",
+        weburl: "https://www.impactlebanon.org/",
+        imgsrc: "assets/imgs/donations/impactlebanon.jpg"
+    },
+    {
+        id: 4,
+        name: "St George Hospital",
+        excerpt: `Saint George Hospital University Medical Center is a nonprofit academic medical center, 
+        owned by the Orthodox Archdiocese of Beirut and is academically affiliated to the University of Balamand. 
+        It is committed to treat all patients with dignity, respect and compassion regardless of their religion, nationality, 
+        socioeconomic status and race. 
+        It provides quality healthcare services to the sick and indigent at the lowest possible cost...`,
+        donateurl: "https://www.stgeorgehospital.org/stgeorge-donation",
+        weburl: "https://www.stgeorgehospital.org/",
+        imgsrc: "assets/imgs/donations/stgeorgehospital.png"
+    },
+    {
+        id: 5,
+        name: "Lebanon of Tomorrow",
+        excerpt: `Lebanon Of Tomorrow is a Lebanese NGO that launched in 2020, 
+        with the objective to provide vulnerable categories of the Lebanese people with social assistance, 
+        particularly in times of economic and financial crises...`,
+        donateurl: "http://lebanonoftomorrow.org/product/donate-for-beirut/",
+        weburl: "https://lebanonoftomorrow.org/",
+        imgsrc: "assets/imgs/donations/LOT.png"
+    },
+    {
+        id: 6,
+        name: "Sesobel",
+        excerpt: `Since 1976, SESOBEL has taken all possible steps to implement a coherent and monitored service of assisting children with disability and to accompany their families as partners in facing life’s challenges.
+        SESOBEL has worked equally hard with all elements of the society to recognize the value, dignity, and respect for children with disability...`,
+        donateurl: "https://www.sesobel.org/en/sengager/faireundon",
+        weburl: "https://www.sesobel.org/",
+        imgsrc: "assets/imgs/donations/sesobel.png"
+    }
+    ,
+    {
+        id: 7,
+        name: "Lebanese Food Bank",
+        excerpt: ``,
+        donateurl: "https://donate.lebanesefoodbank.org/",
+        weburl: "https://lebanesefoodbank.org/",
+        imgsrc: "assets/imgs/donations/lfb.png"
+    },
+    {
+        id: 8,
+        name: "Caritas",
+        excerpt: `Caritas Lebanon, the common official socio-pastoral arm of the local Catholic Church in service of the poor and the promotion of love, 
+        charity and justice, provides economic development, livelihoods, health and social care, education, migration services, 
+        emergency and crisis intervention, human and humanitarian relief and aid, environmental stewardship, 
+        as well as advocacy and protection for all individuals and groups of people in need...`,
+        donateurl: "http://www.caritas.org.lb/get_involved/donate",
+        weburl: "http://www.caritas.org.lb/",
+        imgsrc: "assets/imgs/donations/caritas.png"
+    },
+    {
+        id: 9,
+        name: "Offrejoie",
+        excerpt: `Offrejoie was born in the midst of a raging civil war in Lebanon in 1985. 
+        A group of young Red Cross volunteers worked together to help the injured. 
+        Alleviating human suffering transcended any differences between them. They committed to heal their torn country. 
+        They started with children’s camps during the conflict...`,
+        donateurl: "https://offrejoie.org/donate/",
+        weburl: "https://offrejoie.org/",
+        imgsrc: "assets/imgs/donations/offrejoie.png"
+    },
+    {
+        id: 10,
+        name: "Live Love Rebuild Beirut",
+        excerpt: `We are an independent non-profit founded in 2012 in Beirut and Los Angeles with the goal of sharing, 
+        celebrating and helping the beauty of Lebanon...`,
+        donateurl: "https://livelove.org/donate",
+        weburl: "https://livelove.org/rebuild_beirut",
+        imgsrc: "assets/imgs/donations/liveloverebuildbeirut.png"
+    },
+    {
+        id: 11,
+        name: "Baytna Baytak",
+        excerpt: `In the wake of the tragic Beirut blast, 
+        Baytna Baytak has shifted its priority to relocating displaced individuals who have lost their homes to the blast. 
+        More than 300,000 Lebanese citizens need our assistance. 
+        Please help us help them...`,
+        donateurl: "https://www.justgiving.com/crowdfunding/impact-lebanon?utm_term=dXEJkepP7",
+        weburl: "https://www.baytnabaytak.com/home",
+        imgsrc: "assets/imgs/donations/baytnabaytak.png"
+    },
+    {
+        id: 12,
+        name: "Beit El Baraka",
+        excerpt: `Beit el Baraka is a non-profit organization established in Beirut in 2018, and recognized by the Lebanese state by virtue of Decree No 2277 ...`,
+        donateurl: "https://beitelbaraka.net/donation/pay/",
+        weburl: "https://www.beitelbaraka.org/",
+        imgsrc: "https://www.beitelbaraka.org/wp-content/uploads/2020/06/1-04.png"
+    }
+
+
+
+
+
+
+
+]
